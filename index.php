d101296fb5ff6fc9e717d57acd08cb3bc22ca0b3
Marco <bozicma@gmail.com>
2019-12-29 16:07:24 -0500

    displaying table info on webpage

--- index.php
+++ index.php
@@ -24,6 +24,26 @@ if ($mysqli->connect_errno) {
 
 echo $mysqli->host_info . "\n";
 
+$sql = "SELECT JokeID, Joke_question, Joke_answer FROM Jokes_table";
+$result = $mysqli->query($sql);
+
+if ($result->num_rows > 0)
+{
+	//output data of each row
+	echo "<br>";
+	while($row = $result->fetch_assoc())
+	{
+		echo "id: " . $row["JokeId"] . " - Joke Question: " . $row["Joke_question"] . " " . $row["Joke_answer"] . "<br>";
+	}
+}
+
+else
+{
+	echo "0 results";
+}
+
+$mysqli->close();
+
 ?>
 
 
