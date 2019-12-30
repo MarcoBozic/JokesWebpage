9841706435b918430d1b48187aaa94e727664116
Marco <bozicma@gmail.com>
2019-12-29 22:47:19 -0500

    able to successfully search for jokes from user inputted key word

--- search_keyword.php
+++ search_keyword.php
@@ -8,7 +8,7 @@ echo $keywordfromform;
 // Search the database for the word chicken
 echo "<h2>Show all jokes with the word chicken</h2>";
 
-$sql = "SELECT JokeID, Joke_question, Joke_answer, FROM Jokes_table WHERE Joke_question LIKE '%chicken%'";
+$sql = "SELECT JokeID, Joke_question, Joke_answer, FROM Jokes_table WHERE Joke_question LIKE '% . $keywordfromform . %'";
 $result = $mysqli->query($sql);
 
 if ($result->num_rows > 0)
