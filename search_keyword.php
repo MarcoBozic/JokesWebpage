7e2523e95baeb49e2eadb0ea1ea3c97c1fe3313d
Marco <bozicma@gmail.com>
2019-12-29 22:54:44 -0500

    changed header to reflect user inputted key word

--- search_keyword.php
+++ search_keyword.php
@@ -3,10 +3,10 @@
 include "db_connect.php";
 
 $keywordfromform = $_GET["keyword"];
-echo $keywordfromform;
+//echo $keywordfromform;
 
-// Search the database for the word chicken
-echo "<h2>Show all jokes with the word chicken</h2>";
+// Search the database for the word cthe user requested
+echo "<h2>Show all jokes with the word $keywordfromform</h2>";
 
 $sql = "SELECT JokeID, Joke_question, Joke_answer, FROM Jokes_table WHERE Joke_question LIKE '% . $keywordfromform . %'";
 $result = $mysqli->query($sql);
