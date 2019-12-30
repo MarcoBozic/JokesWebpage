6324bfa89cd5d4be25928329d84c404d663a368c
Marco <bozicma@gmail.com>
2019-12-29 22:07:13 -0500

    added ability for user to search for specific key word

--- index.php
+++ index.php
@@ -12,6 +12,20 @@ include "db_connect.php";
 
 include "search_all_jokes.php";
 
+?>
+
+<form action="/search_keyword.php">
+  Please enter a key word to search for:<br>
+  <input type="text" name="keyword"><br>
+ 
+  <input type="submit" value="Submit">
+</form>
+
+
+<?php
+
+
+
 include "search_keyword.php";
 
 
