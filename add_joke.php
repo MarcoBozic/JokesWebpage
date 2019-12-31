d40190f4ff48b2c9551e51a8b2fb929ed817051b
Marco <bozicma@gmail.com>
2019-12-30 22:39:49 -0500

    used mysqli_error() function to display error if bad user input

--- add_joke.php
+++ add_joke.php
@@ -11,7 +11,7 @@ $new_joke_answer = $_GET["newanswer"];
 echo "<h2>Trying to add a new joke: $new_joke_question and $new_joke_answer</h2>";
 
 $sql = "INSERT INTO Jokes_table (JokeID, Joke_question, Joke_answer) VALUES (NULL, '$new_joke_question', '$new_joke_answer')"
-$result = $mysqli->query($sql);
+$result = $mysqli->query($sql) or die(mysqli_error($mysqli));
 
 include "search_all_jokes.php";
 
