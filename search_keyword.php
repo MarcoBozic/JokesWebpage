9301a55714afa659e3e367269ca270c805edb51a
Marco <bozicma@gmail.com>
2019-12-31 20:11:23 -0500

    used jQuery UI for accordion formatting to make searched joke display look nicer

--- search_keyword.php
+++ search_keyword.php
@@ -1,3 +1,20 @@
+<head>
+
+<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+  <link rel="stylesheet" href="/resources/demos/style.css">
+  <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+  <script>
+  $(function() {
+    $( "#accordion" ).accordion();
+  } );
+  </script>
+</head>
+
+
+
+
+
 <?php
 
 include "db_connect.php";
@@ -11,12 +28,21 @@ echo "<h2>Show all jokes with the word $keywordfromform</h2>";
 $sql = "SELECT JokeID, Joke_question, Joke_answer, FROM Jokes_table WHERE Joke_question LIKE '% . $keywordfromform . %'";
 $result = $mysqli->query($sql);
 
-if ($result->num_rows > 0)
-{
+
+?>
+
+<div id="accordion">
+
+<?php
+
+
+if ($result->num_rows > 0) {
 	//output data of each row
 	while($row = $result->fetch_assoc())
 	{
-		echo "id: " . $row["JokeID"] . " - Joke Question: " . $row["Joke_question"] . " " . $row["Joke_answer"] . "<br>";
+		//echo "id: " . $row["JokeID"] . " - Joke Question: " . $row["Joke_question"] . " " . $row["Joke_answer"] . "<br>";
+		echo "<h3>$row[Joke_question]</h3>";
+		echo "<div><p>$row[Joke_answer]</p></div>";
 	}
 }
 
@@ -25,6 +51,6 @@ else
 	echo "0 results";
 }
 
-
-
 ?>
+
+</div>
