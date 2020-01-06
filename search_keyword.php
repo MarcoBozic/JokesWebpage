5d0a41e47dd2c1ea571e6d3601cdc4a1114b1d6e
Marco <bozicma@gmail.com>
2020-01-06 15:54:34 -0500

    added final comments for search_keyword.php

--- search_keyword.php
+++ search_keyword.php
@@ -14,14 +14,20 @@
 
 <?php
 
+
+/* the purpose of search_keyword.php is to display the jokes
+that cointain a specific key word
+*/
+
+
+//includes functions from db_connect.php
 include "db_connect.php";
 
+//key word variable taken from form
 $keywordfromform = $_GET["keyword"];
-//echo $keywordfromform;
-
 echo "<h1>Looking for jokes that only have the word <em>$keywordfromform</em> in the question<br></h1>";
 
-// Search the database for the word cthe user requested
+// Search the database for word user requested and store it in variable
 $sql = "SELECT JokeID, Joke_question, Joke_answer, FROM Jokes_table WHERE Joke_question LIKE '% . $keywordfromform . %'";
 $result = $mysqli->query($sql);
 
@@ -33,17 +39,17 @@ echo "Select returned $result->num_rows rows of data<br>";
 
 <?php
 
-
+//if database is not empty, display all valid infromation
 if ($result->num_rows > 0) {
 	//output data of each row
 	while($row = $result->fetch_assoc())
 	{
-		//echo "id: " . $row["JokeID"] . " - Joke Question: " . $row["Joke_question"] . " " . $row["Joke_answer"] . "<br>";
 		echo "<h3>$row[Joke_question]</h3>";
 		echo "<div><p>$row[Joke_answer]</p></div>";
 	}
 }
 
+//if database is empty display an error message 
 else
 {
 	echo "0 results";
