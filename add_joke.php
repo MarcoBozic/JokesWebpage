831c89254395df2958184a875abf666ba6acdd29
Marco <bozicma@gmail.com>
2020-01-06 16:14:56 -0500

    added final comment for add_joke.php

--- add_joke.php
+++ add_joke.php
@@ -1,20 +1,29 @@
 <?php
 
+
+/* the purpose of add_joke.php is to allow the ability to insert
+jokes and answers into the database 
+*/
+
+
+//includes functions from db_connect.php
 include "db_connect.php";
 
+//variables that contain the joke and answer the user wants to add
 $new_joke_question = $_GET["newjoke"];
 $new_joke_answer = $_GET["newanswer"];
-//echo $keywordfromform;
-
+//updates the new joke and answer to allow for apostrophes
 $new_joke_question = addslashes($new_joke_question);
 $new_joke_answer = addslashes($new_joke_answer);
 
 // Search the database for the word the user requested
 echo "<h2>Trying to add a new joke: $new_joke_question and $new_joke_answer</h2>";
 
+//insert the new joke and answer into the database
 $sql = "INSERT INTO Jokes_table (JokeID, Joke_question, Joke_answer) VALUES (NULL, '$new_joke_question', '$new_joke_answer')"
 $result = $mysqli->query($sql) or die(mysqli_error($mysqli));
 
+//includes functions from search_all_jokes.php
 include "search_all_jokes.php";
 
 ?>
