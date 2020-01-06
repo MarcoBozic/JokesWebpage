a496ce2d4a591a6ff9b340dd06ad82a6aad9c091
Marco <bozicma@gmail.com>
2020-01-06 15:35:52 -0500

    added final comments for search_all_jokes.php

--- search_all_jokes.php
+++ search_all_jokes.php
@@ -1,5 +1,13 @@
 <?php
 
+
+
+/* the purpose of search_all_jokes.php is to go through all the jokes 
+in the database and to display them, along with the ID and answer, in
+chronological order
+*/
+
+
 //if there are any values in the table, display them one at a time
 if ($mysqli->connect_errno) {
     echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
@@ -8,9 +16,11 @@ if ($mysqli->connect_errno) {
 
 echo $mysqli->host_info . "\n";
 
+//joke content variable taken from database
 $sql = "SELECT JokeID, Joke_question, Joke_answer FROM Jokes_table";
 $result = $mysqli->query($sql);
 
+//if database is not empty, display all valid information
 if ($result->num_rows > 0)
 {
 	//output data of each row
@@ -21,6 +31,7 @@ if ($result->num_rows > 0)
 	}
 }
 
+//if database is empty display an error message 
 else
 {
 	echo "0 results";
